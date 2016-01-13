0101c7160c210c0b504d596db342c49e93613bfa
Brian Carpio <bcarpio@thetek.net>
2016-01-13 12:21:55 -0700

    [master]: added a bunch of tests

--- spec/classes/zabbixserver_spec.rb
+++ spec/classes/zabbixserver_spec.rb
@@ -4,4 +4,8 @@ require_relative '../spec_helper'
 describe 'zabbixserver' do
   let(:facts) { { :osfamily => 'Redhat', :operatingsystemrelease => '6.6', :concat_basedir => '/var/tmp' } }
   it { should compile }
+  it { should contain_class('apache')}
+  it { should contain_class('apache::mod::php')}
+  it { should contain_class('mysql::server')}
+  it { should contain_class('zabbix')}
 end
